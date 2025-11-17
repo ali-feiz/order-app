9260204685c249557be5b57ffc6088ac4f5a4ee3
ali-feiz <alirezafeizabadi1379@gmail.com>
2025-11-17 14:48:36 +0330

    chore: setup router, layout, fonts, and icons
    - Configured Vue Router with default /orders route and proper Layout component
    - Added <router-view> in Layout.vue for child route rendering
    - Imported Material Design Icons (@mdi/font) and set up global CSS
    - Ensured layout and icons display correctly across pages

---
+++ src/layout/Public.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="auth-wrapper">
+    <div>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Public",
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+.auth-wrapper {
+  display: grid;
+  place-items: center; /* Centers content horizontally & vertically */
+  height: 100vh;
+}
+</style>
---
+++ src/views/orders.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>{{ $t("orders.orders") }}</div>
+</template>
